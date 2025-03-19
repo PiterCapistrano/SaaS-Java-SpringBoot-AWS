af263d4e3e9bc6923f66831f56178c6fb33e0164
Piter <pitercapistrano@gmail.com>
2025-03-18 21:29:22 -0300

    Criação do Micro Serviço Gateway Service para Comunicação Entre Front-end e Back-end

---
+++ gateway-service/src/test/java/com/meusaas/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meusaas.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
